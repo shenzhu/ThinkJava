d691e138aef555e3176c9001db32d29cd07bb2be
Shen Zhu <shenzhu0127@gmail.com>
2015-04-22 21:23:34 +0800

    chapter3, ex2

---
+++ AmericanTime.java
@@ -0,0 +1,32 @@
+
+public class AmericanTime {
+
+	/**
+	 * @param args
+	 */
+	public static void printAmerican(int day, String date, String month, int year){
+		System.out.print(date);
+		System.out.print(",");
+		System.out.print(month);
+		System.out.print(" ");
+		System.out.print(day);
+		System.out.print(",");
+		System.out.println(year);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day;
+		String date;
+		String month;
+		int year;
+		
+		day = 16;
+		date = "Saturday";
+		month = "July";
+		year = 2011;
+		
+		printAmerican(day, date, month, year);
+	}
+
+}
